a5b3ab7f411dd6a9d859794049366fa83389386a
BKdD333 <1564211525@qq.com>
2024-07-16 22:50:04 +0800

    prepare redux

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -8,24 +8,21 @@ import { useNavigate } from 'react-router-dom'
 const Login = () => {
   // const dispatch = useDispatch()
   // const navigate = useNavigate()
-  // const onFinish = async (values) => {
-  //   console.log(values)
-  //   // 触发异步action fetchLogin
-  //   await dispatch(fetchLogin(values))
-  //   // 1. 跳转到首页
-  //   navigate('/')
-  //   // 2. 提示一下用户
-  //   message.success('登录成功')
-  // }
+  const onFinish = async (values) => {
+    console.log(values)
+    // 触发异步action fetchLogin
+    // await dispatch(fetchLogin(values))
+    // 1. 跳转到首页
+    // navigate('/')
+    // 2. 提示一下用户
+    message.success('登录成功')
+  }
   return (
     <div className="login">
       <Card className="login-container">
         <img className="login-logo" src={logo} alt="" />
         {/* 登录表单 */}
-        <Form
-          // onFinish={onFinish}
-          validateTrigger="onBlur"
-        >
+        <Form onFinish={onFinish} validateTrigger="onBlur">
           <Form.Item
             name="mobile"
             // 多条校验逻辑 先校验第一条 第一条通过之后再校验第二条
